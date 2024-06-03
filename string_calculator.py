8475097cf06a1f9873c703a5356d18908cd26c6b
hsk0444 <shivanikumar444@gmail.com>
2024-06-04 00:25:56 +0530

    added logic for string calculator along with unit test cases

--- string_calculator.py
+++ string_calculator.py
@@ -7,4 +7,21 @@ class StringCalculator:
         if not numbers:
             return 0
 
+        delimiter = ",|\n"
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = re.escape(parts[0][2:])
+            numbers = parts[1]
+
+        number_list = re.split(delimiter, numbers)
+        sum = 0
+        negatives = []
+
+        for number in number_list:
+            if number:
+                num = int(number)
+                if num < 0:
+                    negatives.append(num)
+                sum += num
+
         return sum
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -10,5 +10,20 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(self.calc.add(""), 0)
 
+    def test_one_number(self):
+        self.assertEqual(self.calc.add("1"), 1)
+
+    def test_two_numbers(self):
+        self.assertEqual(self.calc.add("1,2"), 3)
+
+    def test_multiple_numbers(self):
+        self.assertEqual(self.calc.add("1,2,3,4"), 10)
+
+    def test_new_lines_between_numbers(self):
+        self.assertEqual(self.calc.add("1\n2,3"), 6)
+
+    def test_different_delimiter(self):
+        self.assertEqual(self.calc.add("//;\n1;2"), 3)
+
 if __name__ == "__main__":
     unittest.main()
